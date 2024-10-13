15bae4b9e26218719952c808340293546711a369
Mauro Castillo III <mauro.m.castillo3@gmail.com>
2024-10-13 15:07:10 -0500

    Initial commit

---
+++ app/src/main/java/com/android/fetch_rewards_2024/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.android.fetch_rewards_2024
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var navHostFragment: NavHostFragment
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+    }
+}
